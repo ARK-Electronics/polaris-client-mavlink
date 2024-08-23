51bf3166ab737e51d244f0a6ea439be0bd04d14d
Jacob Dahl <dahl.jakejacob@gmail.com>
2024-08-23 13:36:06 -0800

    use install script instead of CMake

---
+++ install.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Setup XDG default paths
+DEFAULT_XDG_CONF_HOME="$HOME/.config"
+DEFAULT_XDG_DATA_HOME="$HOME/.local/share"
+export XDG_CONFIG_HOME="${XDG_CONFIG_HOME:-$DEFAULT_XDG_CONF_HOME}"
+export XDG_DATA_HOME="${XDG_DATA_HOME:-$DEFAULT_XDG_DATA_HOME}"
+THIS_DIR="$(dirname "$(realpath "$BASH_SOURCE")")"
+
+sudo true
+
+# Install dependencies
+sudo apt-get install -y libssl-dev libgflags-dev libgoogle-glog-dev libboost-all-dev
+
+make
+
+# Setup project directory
+sudo cp $THIS_DIR/build/polaris-client-mavlink /usr/local/bin
+mkdir -p $XDG_DATA_HOME/polaris-client-mavlink
+cp $THIS_DIR/config.toml $XDG_DATA_HOME/polaris-client-mavlink/
+
+# Modify config file if ENV variables are set
+CONFIG_FILE="$XDG_DATA_HOME/polaris-client-mavlink/config.toml"
+
+if [ -n "$POLARIS_API_KEY" ]; then
+	echo "Setting polaris_api_key to: $POLARIS_API_KEY"
+	sed -i "s/^polaris_api_key = \".*\"/polaris_api_key = \"$POLARIS_API_KEY\"/" "$CONFIG_FILE"
+fi
